bf22ddb6db45d2bed54eadfb37faa38bc632b55f
nzemia <mualukofrank@gmail.com>
2024-08-09 09:46:09 +0300

    following feed

---
+++ src/app/(main)/FollowingFeed.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import InfiniteScrollContainer from "@/components/InfiniteScrollContainer";
+import DeletePostDialog from "@/components/posts/DeletePostDialog";
+import Post from "@/components/posts/Post";
+import PostsLoadingSkeleton from "@/components/posts/PostsLoadingSkeleton";
+import { Button } from "@/components/ui/button";
+import kyInstance from "@/lib/ky";
+import { PostData, PostsPage } from "@/lib/types";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { Loader2 } from "lucide-react";
+
+export default function FollowingFeed() {
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    isFetching,
+    status,
+  } = useInfiniteQuery({
+    queryKey: ["post-feed", "following"],
+    queryFn: ({ pageParam }) =>
+      kyInstance
+        .get(
+          "/api/posts/following",
+          pageParam ? { searchParams: { cursor: pageParam } } : {},
+        )
+        .json<PostsPage>(),
+    initialPageParam: null as string | null,
+    getNextPageParam: (lastPage) => lastPage.nextCursor,
+  });
+
+  const posts = data?.pages.flatMap((page) => page.posts) ?? [];
+
+  if (status === "pending") {
+    return <PostsLoadingSkeleton />;
+  }
+
+  if (status === "success" && !posts.length && !hasNextPage) {
+    return <p className="text-center text-muted-foreground">No posts found! Start following some users to see their posts here.</p>;
+  }
+
+  if (status === "error") {
+    return (
+      <p className="text-center text-destructive">
+        An error occurred while loading posts!
+      </p>
+    );
+  }
+
+  return (
+    <InfiniteScrollContainer
+      className="space-y-5"
+      onBottomReached={() => hasNextPage && !isFetching && fetchNextPage()}
+    >
+      {posts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+
+      {/* loading for more posts...  */}
+      {isFetchingNextPage && (
+        <Loader2 className="mx-auto my-4 h-8 w-8 animate-spin" />
+      )}
+    </InfiniteScrollContainer>
+  );
+}
