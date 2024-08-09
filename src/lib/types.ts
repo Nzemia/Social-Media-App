aa1cd80a07d6dd59e2eec0c2849a4bb5e27032a6
nzemia <mualukofrank@gmail.com>
2024-08-09 10:19:06 +0300

    user profile ui

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -6,6 +6,9 @@ export function getUserDataSelect(loggedInUserId: string) {
     username: true,
     displayName: true,
     avatarUrl: true,
+    bio: true,
+    createdAt: true,
+
     followers: {
       where: {
         followerId: loggedInUserId,
@@ -16,11 +19,16 @@ export function getUserDataSelect(loggedInUserId: string) {
     },
     _count: {
       select: {
+        posts: true,
         followers: true,
       },
     },
   } satisfies Prisma.UserSelect;
 }
+
+export type UserData = Prisma.UserGetPayload<{
+  select: ReturnType<typeof getUserDataSelect>;
+}>;
 
 export function getPostDataInclude(loggedInUserId: string) {
   return {
